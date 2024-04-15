77ad226150a7d87fc69325f41b9d5eb5a0000eae
DSi <mh.emon243530@gmail.com>
2024-04-15 14:47:22 +0600

    updated all the thing

--- todo/src/main/java/com/example/todo/controller/TodoController.java
+++ todo/src/main/java/com/example/todo/controller/TodoController.java
@@ -2,10 +2,13 @@ package com.example.todo.controller;
 
 import com.example.todo.entity.Todo;
 import com.example.todo.service.TodoService;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -16,6 +19,9 @@ public class TodoController {
     private List<Todo> todos;
     private Long currentId = (long) -1;
 
+    private int currentPage = 0;
+
+
     public TodoController(TodoService todoService) {
         this.todoService = todoService;
         this.todos = todoService.getAllTodos();
@@ -23,47 +29,148 @@ public class TodoController {
 
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("mainBodyTitle", "All Tasks is : " + todos.size());
         model.addAttribute("todos", todos);
 
         model.addAttribute("newTodo", new Todo());
 
+        model.addAttribute("currentPage" , 1);
+
+        this.currentPage = 1;
+
         return "index";
+    }
+
+    @GetMapping("/today")
+    public String today(Model model) {
+
+        List<Todo> todosDueToday = new ArrayList<>();
+        LocalDate today = LocalDate.now();
+
+        for (Todo todo : todos) {
+            if (todo.getDueDate().isEqual(today)){
+                todosDueToday.add(todo);
+            }
+        }
+
+        model.addAttribute("mainBodyTitle", "Today's Total Tasks is : " + todosDueToday.size());
+
+        model.addAttribute("todos", todosDueToday);
+
+        model.addAttribute("newTodo", new Todo());
+
+        model.addAttribute("currentPage" , 0);
+
+
+        this.currentPage = 0;
+
+        return "todays";
+    }
+
+    @GetMapping("/completed")
+    public String completed(Model model) {
+
+        List<Todo> todoCompleted = new ArrayList<>();
+
+        for (Todo todo : todos) {
+            if (todo.getCompleted()){
+                todoCompleted.add(todo);
+            }
+        }
+
+        model.addAttribute("mainBodyTitle", "Completed Tasks is : " + todoCompleted.size());
+
+        model.addAttribute("todos", todoCompleted);
+
+        model.addAttribute("newTodo", new Todo());
+
+        model.addAttribute("currentPage" , 2);
+
+        this.currentPage = 2;
+
+        return "completed";
+    }
+
+    @GetMapping("/pending")
+    public String pending(Model model) {
+
+        List<Todo> todosPending = new ArrayList<>();
+
+        for (Todo todo : todos) {
+            if (!todo.getCompleted()){
+                todosPending.add(todo);
+            }
+        }
+
+        model.addAttribute("mainBodyTitle", "Pending Tasks is : " + todosPending.size());
+
+        model.addAttribute("todos", todosPending);
+
+        model.addAttribute("newTodo", new Todo());
+
+        model.addAttribute("currentPage" , 3);
+
+        this.currentPage = 3;
+
+
+        return "pending";
     }
 
     @GetMapping("/all")
     private String getAllTodos(){
         todos = todoService.getAllTodos();
+        this.currentPage = 1;
         return "redirect:/";
     }
 
     @PostMapping("/createNewTodo")
     public String saveTodo(@ModelAttribute("newTodo") Todo newTodo ) {
+        Todo current = this.todoService.getTodoById(currentId);
         if (currentId!=-1){
             newTodo.setId(currentId);
-//            System.out.println(newTodo.getTitle());
-//            System.out.println(newTodo.getId());
-//            System.out.println(newTodo.getDescription());
-//            System.out.println(newTodo.getCompleted());
-//            System.out.println(newTodo.getImportant());
-//            System.out.println(newTodo.getDir());
+            newTodo.setCompleted(current.getCompleted());
         }
         else{
             newTodo.setCompleted(false);
         }
         currentId =(long) -1;
         this.todoService.saveTodoToDatabase(newTodo);
+
+        todos = todoService.getAllTodos();
+
+        if (this.currentPage==0) return "redirect:/today";
+        else if (this.currentPage==2) return "redirect:/completed";
+        else if (this.currentPage==3) return  "redirect:/pending";
+
         return "redirect:/all";
     }
 
-    @PostMapping("/updateTodo")
-    public String updateCurrentTodo(@ModelAttribute("newTodo") Todo newTodo ) {
-        this.todoService.updateTodoToDatabase(newTodo);
-        return "redirect:/all";
-    }
 
     @GetMapping("/delete/{id}")
     public String deleteTodo(@PathVariable Long id) {
         this.todoService.deleteTodoFromDatabase(id);
+
+        todos = todoService.getAllTodos();
+
+        if (this.currentPage==0) return "redirect:/today";
+        else if (this.currentPage==2) return "redirect:/completed";
+        else if (this.currentPage==3) return  "redirect:/pending";
+
+        return "redirect:/all";
+    }
+
+    @PostMapping("/update-completed/{id}")
+    public String updateCompleted(@PathVariable Long id , Model model){
+        Todo currentTodo = this.todoService.getTodoById(id);
+        currentTodo.setCompleted(!currentTodo.getCompleted());
+        this.todoService.updateTodoToDatabase(currentTodo);
+
+        todos = todoService.getAllTodos();
+
+        if (this.currentPage==0) return "redirect:/today";
+        else if (this.currentPage==2) return "redirect:/completed";
+        else if (this.currentPage==3) return  "redirect:/pending";
+
         return "redirect:/all";
     }
 
@@ -72,9 +179,7 @@ public class TodoController {
     public String updateTodo(@PathVariable Long id, Model model) {
         currentId = id;
         Todo updatedTodo = this.todoService.getTodoById(id);
-        model.addAttribute("newTodo", updatedTodo); // Change here
-//        Todo todo = (Todo) model.getAttribute("newTodo");
-//        System.out.println("Current Todo Title: " + todo.getTitle());
+        model.addAttribute("newTodo", updatedTodo);
 
         return " "+ //
                 "<div class=\"form-group mb-3\">\n" +
