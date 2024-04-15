f71a461961f2ee6910a45502aea78fdabc17b625
Scorpius72 <mh.emon243530@gmail.com>
2024-04-15 15:15:03 +0600

    liquibase added

--- todo/src/main/java/com/example/todo/service/TodoService.java
+++ todo/src/main/java/com/example/todo/service/TodoService.java
@@ -1,13 +1,19 @@
 package com.example.todo.service;
 
 import com.example.todo.entity.Todo;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 
 public interface TodoService {
     List<Todo> getAllTodos();
     Todo getTodoById(Long id);
+
+    @Transactional
     Todo saveTodoToDatabase(Todo todo);
+    @Transactional
     Todo updateTodoToDatabase(Todo todo);
+
+    @Transactional
     void deleteTodoFromDatabase(Long id);
 }
